11a6d5cea91f220c30ac61d4ba1fcac4d18eda6a
codeReaper2001 <2210086948@qq.com>
2022-07-12 00:08:01 +0800

    [style]精简代码

--- example-client/src/main/java/github/codeReaper2001/NettyClientMain.java
+++ example-client/src/main/java/github/codeReaper2001/NettyClientMain.java
@@ -2,11 +2,8 @@ package github.codeReaper2001;
 
 import github.codeReaper2001.config.ClientConfig;
 import github.codeReaper2001.controller.IndexController;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-// 当前版本用于测试，还未编写完成
-@Slf4j
 public class NettyClientMain {
 
     public static void main(String[] args) {
